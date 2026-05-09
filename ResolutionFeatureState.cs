e0a08e111f6223c6c3d0669c6daaed16f7de8c97
onehoon <onehoon@gmail.com>
2026-05-09 10:02:53 +0900

    fix: Add missing using directive for Windows.Storage.FileProperties

--- ResolutionFeatureState.cs
+++ ResolutionFeatureState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Storage.FileProperties;
 
 namespace Easy_Shortcut_for_UMPC
 {
